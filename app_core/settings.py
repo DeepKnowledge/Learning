0f38002c1145517fe8bdfaecd69845d8e775937c
Ryan Wang <wanglun02@gmail.com>
2012-06-04 22:18:36 -0500

    renamed project

--- app_core/settings.py
+++ app_core/settings.py
@@ -104,10 +104,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'django1.urls'
+ROOT_URLCONF = 'app_core.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'django1.wsgi.application'
+WSGI_APPLICATION = 'app_core.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- app_core/wsgi.py
+++ app_core/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django1.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_core.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
